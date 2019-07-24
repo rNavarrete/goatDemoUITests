d7cde2f7ca40e8c8c826c33d6488bba87140c48b
rolando <kinglouis.belmont@gmail.com>
2019-07-23 21:42:54 -0700

    Initial Commit

---
+++ GoatAppDemoUITests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoatAppDemoUITests
+//
+//  Created by Rolando Navarrete on 7/23/19.
+//  Copyright © 2019 Rolando Navarrete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
